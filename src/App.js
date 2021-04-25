b35e68d8989c8dfa2af4353b69ab54f6b463c546
acrosight <orjans@live.no>
2021-04-25 17:04:43 +0200

    Added button for identificcation session

--- src/App.js
+++ src/App.js
@@ -2,15 +2,19 @@ import './App.css';
 import React, {useState} from 'react'
 
 function App(){
+  // Variables to store important information
+  const [id, setId] = useState('')
   const [identity, setIdentity] = useState(null)
 
 
   async function getToken() {
+    // The client_id and client_secret
     const client_id = "t8750fe84a1e843b2b97be9ff88830d09"
     const client_secret = "1gq3CibSqZPpj2jKUjfcMPcA9Eoryj8bcMkgcrsgPrxHiNs5N94VplJpLGAWcODo"
 
     // Turning into HTML Basic Authentication with base-64 formatting
     const authorizationBasic = `Basic ` + btoa(`${client_id}:${client_secret}`);
+    console.log(authorizationBasic)
 
     let header = {
       'Authorization': authorizationBasic,
@@ -19,25 +23,23 @@ function App(){
 
     const payload = { "grant_type": "client_credentials", "scope": "identify" }
 
-    console.log(authorizationBasic)
-
-    console.log(header)
     const res = await fetch("https://api.signicat.io/oauth/connect/token", {
         method: "POST",
         mode: 'no-cors',
         headers: header,
         body: JSON.stringify(payload),
     })
+
     console.log(res.status)
-    const data = await res.json();
-    //console.log(data.access_token);
-    return data.access_token
+    // const data = await res.json();
+    // console.log(data.access_token);
+    // return data.access_token
   }
 
   async function getId() {
     // Retrieving a token
     const token = getToken();
-    // Standard payload
+    // Standard request 
     const payload = {
       "flow": "redirect",
       "allowedProviders": ["no_bankid_netcentric", "no_bankid_mobile"],
@@ -58,8 +60,8 @@ function App(){
           'Content-Type': 'application/json',
         },
         body: JSON.stringify(payload),
-      }
-    );
+      });
+
     console.log(res.status);
     const data = await res.json();
     console.log(data);
@@ -67,12 +69,13 @@ function App(){
     // Opening the url in a popup window
     window.open(data.url);
 
+    setId(data.id)
     return data.id
   }
 
-  // Saving the identification seesion
-  async function buttonPress() {
-    const id = await getId();
+  // Saving the identification session
+  async function getIdentity() {
+    // Retrieving the token
     const token = await getToken();
     const res = await fetch (`https://api.signicat.io/identification/v2/sessions/${id}`, 
     {
@@ -86,24 +89,27 @@ function App(){
     setIdentity(res.identity)
   }
 
-
-    if(identity=== null){
-      return (
-        <div className="App">
-          <header className="header">Press the button to create a Identification session</header>
-          <input className="btn" type="button" value="Fetch" onClick={getId}/>
+  // If the user has not logged in
+  if(identity===null){
+    return (
+      <div className="App">
+        <header className="header">Press the button to create a Identification session</header>
+        {/* Used to test the getToken method, but will be replaced with getId */}
+        <div className="buttonWrapper">
+          <input className="btn" type="button" value="Authenticate" onClick={getToken}/>
+          <input className="btn" type="button" value="Get info" onClick={getIdentity}/>
         </div>
-      );
-    } else {
-      return(
-        <div className="App">
-          <ul>
-
-          </ul>
-        </div>
-      )
-    }
-    
+      </div>
+    );
+  } else {
+    return(
+      <div className="App">
+        <text>Name: {identity.fullName}</text>
+        <text>Date of Birth: {identity.dateOfBirth}</text>
+      </div>
+    )
   }
+  
+}
 
 export default App;
